181103ff5a462c15ea68706b1a1b260fa471e385
SpiDher <penivera655@gmail.com>
2025-06-24 15:34:59 +0100

    added a project

---
+++ uyy-csc221/python-scripts/something.py
@@ -0,0 +1,16 @@
+import math
+pi = math.pi
+w = -0.56
+lamda = 4.8
+n =8
+t = 6.09
+v = -3.56
+q =2
+b = 7
+p1 = 0.45
+q1 = 3.46
+v2 = 8
+r1 = lamda
+beta = math.sin(45)
+k = beta * lamda
+print(pow(((pi/2)+(2*v*q)),(1/lamda)))
